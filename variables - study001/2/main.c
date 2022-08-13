3bbe842b7d570b5c1872500d5461dd918745bfc0
Vinícius Gonçalves <oldz.registros@gmail.com>
2022-08-12 22:32:53 -0300

    Added more studies

--- variables - study001/2/main.c
+++ variables - study001/2/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 void main() {
+	
+	#define name "VINICIUS"
 	
 	int number = 10;
 	char genre = 'M';
 	char string[] = "Hello World";
+	float numberTwo = 19.5;
 	
 	printf("%d\n", sizeof(number));
 	printf("%d\n", sizeof(genre));
 	printf("%d\n", sizeof(string));
+	printf("%d\n", sizeof(numberTwo));
+	printf("%d\n", sizeof(name));
+	
+	printf("%d", number);
 }
